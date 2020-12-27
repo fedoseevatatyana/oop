bd7ed5be219e80ede5ecbdc122c25e2f099d0a5c
Tatyana <fedoseevatatyana9@gmail.com>
2020-12-27 17:27:59 +0300

    task 2

---
+++ src/main/java/PostManager.java
@@ -0,0 +1,15 @@
+public class PostManager {
+    private Post[] search();
+    private int ownerId;
+    private int domain;
+    private String query;
+    public String generateBlock() {
+        return null;
+    }
+    private void delete();
+    private int ownerId;
+    private int postId;
+    public String generateBlock() {
+        return null;
+    }
+}
